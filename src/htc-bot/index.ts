8132ead68091d3ba09792b0fd873a0751a51d7e7
vladtims <timofeev@engee.cloud>
2024-12-20 17:06:25 +0300

    HTC-1: Learned how create custom buttons

--- src/htc-bot/index.ts
+++ src/htc-bot/index.ts
@@ -1,11 +1,9 @@
-import { Bot }                         from 'grammy';
-import { loggingMiddleware }           from "./middlewares/logging";
-import { startCommand }                from "./commands/start";
-import dotenv                          from 'dotenv';
-import { getUserFirstNameFrom }        from "./helpers/user-info.helpers";
-import { transformTextToItalicHelper } from "./helpers/markdown.helpers";
-import { getErrorHeler }               from "./helpers/errors-catch.helpers";
-import { htcBotCommands }              from "./constants/commands";
+import { Bot }               from 'grammy';
+import { loggingMiddleware } from "./middlewares/logging";
+import { useStartCommand }   from "./commands/start";
+import dotenv                from 'dotenv';
+import { getErrorHeler }     from "./helpers/errors-catch.helpers";
+import { useMassageHandler } from "./handlers/message.handler";
 
 dotenv.config();
 
@@ -13,21 +11,25 @@ const htcBot = new Bot(process.env.TOKEN);
 
 htcBot.use(loggingMiddleware);
 
-htcBot.api.setMyCommands(htcBotCommands);
+void useStartCommand(htcBot);
+void useMassageHandler(htcBot);
 
-htcBot.command('start', startCommand);
 
-htcBot.on("message", async (ctx) => {
-	const userText = ctx.message.text;
-	const author = await ctx.getAuthor();
-	const userFirstName = getUserFirstNameFrom(author.user);
-
-	await ctx.api.sendMessage(ctx.message.chat.id, `
-    Я еще очень мало умею, но постарался понять, что вы написали, ${userFirstName}\\.
-    \nКажется, вы написали:
-    \n${transformTextToItalicHelper(userText)}`, {parse_mode: "MarkdownV2"}
-	)
-});
+// htcBot.api.setMyCommands(htcBotCommands);
+//
+// htcBot.command('start', startCommand);
+//
+// htcBot.on("message", async (ctx) => {
+// 	const userText = ctx.message.text;
+// 	const author = await ctx.getAuthor();
+// 	const userFirstName = getUserFirstNameFrom(author.user);
+//
+// 	await ctx.api.sendMessage(ctx.message.chat.id, `
+//     Я еще очень мало умею, но постарался понять, что вы написали, ${userFirstName}\\.
+//     \nКажется, вы написали:
+//     \n${transformTextToItalicHelper(userText)}`, {parse_mode: "MarkdownV2"}
+// 	)
+// });
 
 htcBot.catch((botError) => {
 	const error = getErrorHeler(botError);
