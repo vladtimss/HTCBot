edbbfc86e2429e0c206f68f5e1ddea5700363167
vladtims <timofeev@engee.cloud>
2024-12-26 20:38:31 +0300

    HTC-3: Remove attempt with yandex navigator buttons

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -7,7 +7,6 @@ import {
 	UPCOMING_CHURCH_EVENTS
 }                                           from "../strings/keyboards/start.keyboards.strings";
 import { Divider }                          from "../ui-elements/html.elements";
-import { log }                              from "../middlewares/logger";
 import { HTC_TG_CHANNEL_LINK }              from "../constants/links.constants";
 import { generateCalendarEventMessage }     from "../messages/calendar.messages";
 import { getUpcomingCalendarEvents }        from "../helpers/calendar.helpers";
@@ -31,16 +30,8 @@ composer.command(Command.START, (ctx) => {
 composer.hears([CHURCH_TG_CHANNEL, SUNDAY_WORSHIP_INFO, UPCOMING_CHURCH_EVENTS], async (ctx) => {
 	switch (ctx.message.text) {
 		case SUNDAY_WORSHIP_INFO:
-			await ctx.reply(generateSundayWorshipInfoMessage(), {
+			return await ctx.reply(generateSundayWorshipInfoMessage(), {
 				parse_mode: ParseMode.HTML
-			});
-
-			const inlineKeyboard = new InlineKeyboard()
-				.text('Да', "yesOption")
-				.text('Нет', "noOption");
-
-			return await ctx.reply("Хотите, я попробую построить для вас маршрут?", {
-				reply_markup: inlineKeyboard,
 			});
 		case CHURCH_TG_CHANNEL:
 			return await ctx.reply('Посетите канал церкви', {
@@ -61,27 +52,7 @@ composer.hears([CHURCH_TG_CHANNEL, SUNDAY_WORSHIP_INFO, UPCOMING_CHURCH_EVENTS],
 	}
 })
 
-composer.callbackQuery("noOption", async (ctx) => {
-	await ctx.answerCallbackQuery();
-
-	await ctx.reply("Хорошо 😊. Будем рады увидеть вас в нашем собрании 🥰");
-});
-
-composer.callbackQuery("yesOption", async (ctx) => {
-	await ctx.answerCallbackQuery();
-
-	return await ctx.reply('Построить маршрут', {
-		reply_markup: new InlineKeyboard().url(
-			'Открыть навигатор',
-			'https://yandex.ru/navi?rtext=55.485407,37.275574~55.510665,37.349650&rtt=auto'
-		),
-		parse_mode: ParseMode.HTML
-	});
-
-});
-
 composer.on('message:text', async (ctx) => {
-	log('some')
 	return ctx.reply(`${ctx.from.first_name}, я не знаю, что ответить.`)
 });
 
