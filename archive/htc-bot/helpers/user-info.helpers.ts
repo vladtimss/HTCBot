0beb52da2c6c340ed3e046c40c60e8954751bd9f
vladtimss <117174291+vladtimss@users.noreply.github.com>
2024-12-26 18:06:10 +0300

    Delete archive directory
    Delete archive

--- archive/htc-bot/helpers/user-info.helpers.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "@grammyjs/types/manage";
-
-/**
- * Получает имя пользователя или его username
- * @param user
- */
-export const getUserFirstNameFrom = (user: User): string => {
-    return user.first_name ?? user.username;
-}
--- archive/htc-bot/middlewares/logging.ts
+++
@@ -1,7 +0,0 @@
-// Example: src/bot/middlewares/logging.ts
-import { MiddlewareFn } from 'grammy';
-
-export const loggingMiddleware: MiddlewareFn = async (ctx, next) => {
-    console.log(`Handling update ${ctx.update.update_id}`);
-    await next();
-};
